4ad6a4ccc12b1a6bb1295f42d4c9eebe8ee09b93
Chandu909092 <rohitsingh887495@gmail.com>
2024-08-15 14:47:43 +0530

    Update noprefixmhakal.js

--- Priyansh/commands/noprefixmhakal.js
+++ Priyansh/commands/noprefixmhakal.js
@@ -12,7 +12,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Maha")==0 || event.body.indexOf("Har har")==0 || event.body.indexOf("Bhole")==0 || event.body.indexOf("Shiv")==0) {
+	if (event.body.indexOf("mahadev")==0 || event.body.indexOf("Har har")==0 || event.body.indexOf("Bhole")==0 || event.body.indexOf("Shiv")==0) {
 		var msg = {
 				body: "𝙷𝚊𝚛 𝚑𝚊𝚛 𝚖𝚊𝚑𝚊𝚍𝚎𝚟♥️ 𝙹𝚊𝚒 𝚖𝚊𝚑𝚊𝚔𝚊𝚕🌹",
 				attachment: fs.createReadStream(__dirname + `/noprefix/mahakal.mp4`)
